eba63ca8cb2a504053f8005314fba24ac7be2a2b
bap <bap@baps-iMac.local>
2022-10-04 16:27:31 +0700

    binding

---
+++ index.js
@@ -0,0 +1,30 @@
+// sap.ui.define([
+
+// ], function () {
+// 	"use strict";
+
+// 	console.log(sap.ui);
+// 	alert("UI5 is ready");
+// });
+// sap.ui.define([
+// 	"sap/m/Text",
+
+// ], function (Text) {
+// 	"use strict";
+
+// 	new Text({
+// 		text: "Hello World 1111111"
+// 	}).placeAt("content");
+// });
+sap.ui.define([
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
+	"use strict";
+
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
+
+});
